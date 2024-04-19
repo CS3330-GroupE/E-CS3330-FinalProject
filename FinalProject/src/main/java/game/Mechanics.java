48565cf303e4896f56e1cb686f5dbf86f5c01fc9
bsimpson0 <91200121+bsimpson0@users.noreply.github.com>
2024-04-19 14:48:45 -0500

    Corrected .xml error, Created Game classes

--- FinalProject/src/main/java/game/Mechanics.java
+++ FinalProject/src/main/java/game/Mechanics.java
@@ -1,5 +1,34 @@
 package game;
 
 public class Mechanics {
+	
+	
+	//starts game logic loop
+	public void startGame() {
+		
+	}
+	
+	
+	//stops game from running once getUserInput() detects "quit" command
+	private void quitGame() {
+		
+	}
+	
+	
+	//gets commands from user through a scanner
+	private void getUserInput() {
+		
+	}
 
+	
+	//displays info resulting from battleManager() -> damage done, whether attack missed, hp remaining, etc.
+	private void displayInfo() {
+		
+	}
+	
+	
+	//manages all information during player battle -> who's turn, damage calculation, etc.
+	private void battleManager() {
+		
+	}
 }
