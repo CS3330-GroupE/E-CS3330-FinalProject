9144ec1413917a3bfbfb869d1ce84c410203275d
Brian Bluhm <116319535+BDBluhm@users.noreply.github.com>
2024-04-26 08:50:32 -0500

    Update Monster.java
    Created monster superclass

--- FinalProject/src/main/java/monster/Monster.java
+++ FinalProject/src/main/java/monster/Monster.java
@@ -1,5 +1,28 @@
 package monster;
 
-public class Monster {
+import java.util.Random;
 
-}
+import item.Item;
+import item.RandomEquipmentGenerator;
+
+public class Monster {
+	private Random rng;
+	private String name;
+	//hit point value of monster
+    private int health;
+    //determines monster stat bonuses
+    private int level;
+    //value monster must beat on attack roll
+    private int attack;
+    //damage dealt to player on successful roll
+    private int damage;
+    //value player must beat to deal damage
+    private int defense;
+
+    public Monster(String name, int level, int health, int attack, int damage, int defense) {
+        this.name = name;
+        this.level = level;
+        this.health = health;
+        this.attack = attack;
+        this.defense = defense;
+    }
