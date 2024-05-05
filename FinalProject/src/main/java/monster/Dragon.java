111c21e6f13dad0aa1d5d9811c408cb582beee08
Brian Bluhm <116319535+BDBluhm@users.noreply.github.com>
2024-05-05 01:01:12 -0500

    Update Dragon.java

--- FinalProject/src/main/java/monster/Dragon.java
+++ FinalProject/src/main/java/monster/Dragon.java
@@ -3,8 +3,8 @@ package monster;
 
 public class Dragon extends Monster {
 
-	public Dragon(String name, int level, int health, int attack, int damage, int defense) {
-		super("Dragon", 2, 35, 7, 12, 3);
+	public Dragon(String name, int level, int health, int attack, int damage, int defense, String title) {
+		super("Dragon", 2, 35, 7, 12, 3, "wild");
 		// TODO Auto-generated constructor stub
 	}
 
