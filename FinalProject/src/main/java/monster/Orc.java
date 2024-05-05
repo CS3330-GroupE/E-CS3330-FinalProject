70592647d6a2003a6edc2117ccc02903a74e2147
Brian Bluhm <116319535+BDBluhm@users.noreply.github.com>
2024-05-05 01:02:36 -0500

    Update Orc.java
    altered monster subclass

--- FinalProject/src/main/java/monster/Orc.java
+++ FinalProject/src/main/java/monster/Orc.java
@@ -1,8 +1,8 @@
 package monster;
 
 public class Orc extends Monster {
-    public Orc(String name, int level, int health, int attack, int damage, int defense) {
-        super("Orc", 1, 20, 6, 8, 2);
+    public Orc(String name, int level, int health, int attack, int damage, int defense, String title) {
+        super("Orc", 1, 20, 6, 8, 2, "wild");
     }
 
 }
