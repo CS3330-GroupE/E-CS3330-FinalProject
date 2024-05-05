cb244ca4dc5a4ed2ac1667f68e4174aa8ad48c0f
Brian Bluhm <116319535+BDBluhm@users.noreply.github.com>
2024-05-05 01:02:04 -0500

    Update Goblin.java
    altered subclass constructor

--- FinalProject/src/main/java/monster/Goblin.java
+++ FinalProject/src/main/java/monster/Goblin.java
@@ -2,8 +2,8 @@ package monster;
 
 public class Goblin extends Monster {
     
-	public Goblin(String name, int level, int health, int attack, int damage, int defense) {
-        super("Goblin", 0, 8, 5, 3, 3);
+	public Goblin(String name, int level, int health, int attack, int damage, int defense, String title) {
+        super("Goblin", 0, 8, 5, 3, 3, "wild");
     }
     
     //Goblin special mechanics
