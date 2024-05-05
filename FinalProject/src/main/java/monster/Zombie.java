ba596e3906cf2015c126bbc9aeb413d40fb21593
Brian Bluhm <116319535+BDBluhm@users.noreply.github.com>
2024-05-05 01:03:03 -0500

    Update Zombie.java
    altered monster subclass

--- FinalProject/src/main/java/monster/Zombie.java
+++ FinalProject/src/main/java/monster/Zombie.java
@@ -3,8 +3,8 @@ package monster;
 public class Zombie extends Monster {
 	
 	
-    public Zombie(String name, int level, int health, int attack, int damage, int defense) {
-        super("Zombie", 0, 15, 5, 5, 0);
+    public Zombie(String name, int level, int health, int attack, int damage, int defense, String title) {
+        super("Zombie", 0, 15, 5, 5, 0, "wild");
     }
     
     //Zombie special mechanics
