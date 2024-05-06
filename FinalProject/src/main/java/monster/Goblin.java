a7dd0664fcab5ee6184ce2b41ede41d701286120
Brian Bluhm <116319535+BDBluhm@users.noreply.github.com>
2024-05-06 04:25:37 -0500

    Update Goblin.java
    Comment cleanup

--- FinalProject/src/main/java/monster/Goblin.java
+++ FinalProject/src/main/java/monster/Goblin.java
@@ -5,7 +5,4 @@ public class Goblin extends Monster {
 	public Goblin(String name, int level, int health, int attack, int damage, int defense, String title) {
         super("Goblin", 0, 8, 5, 3, 3, "wild");
     }
-    
-    //Goblin special mechanics
-
 }
