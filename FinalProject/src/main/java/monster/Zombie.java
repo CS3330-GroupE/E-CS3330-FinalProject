b4bcf129865a915d09202717460dd2bb5d98c77d
Brian Bluhm <116319535+BDBluhm@users.noreply.github.com>
2024-05-06 04:25:50 -0500

    Update Zombie.java
    Comment cleanup

--- FinalProject/src/main/java/monster/Zombie.java
+++ FinalProject/src/main/java/monster/Zombie.java
@@ -6,7 +6,4 @@ public class Zombie extends Monster {
     public Zombie(String name, int level, int health, int attack, int damage, int defense, String title) {
         super("Zombie", 0, 15, 5, 5, 0, "wild");
     }
-    
-    //Zombie special mechanics
-
 }
